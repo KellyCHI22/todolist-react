a6c197c260bd7402181f65ef5760b32f57027eb3
Hsin-tingCHI <chihsinting@gmail.com>
2023-03-02 14:29:14 +0100

    feat: add change mode handler

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -103,7 +103,9 @@ const StyledTaskItem = styled.div`
 
 const TodoItem = ({ todo, onSave, onDelete, onToggleDone, onChangeMode }) => {
   return (
-    <StyledTaskItem className={clsx('', { done: todo.isDone })}>
+    <StyledTaskItem
+      className={clsx('', { done: todo.isDone, edit: todo.isEdit })}
+    >
       <div className="task-item-checked">
         <span
           className="icon icon-checked"
@@ -112,9 +114,14 @@ const TodoItem = ({ todo, onSave, onDelete, onToggleDone, onChangeMode }) => {
           }}
         />
       </div>
-      <div className="task-item-body">
+      <div
+        className="task-item-body"
+        onDoubleClick={() => {
+          onChangeMode?.({ id: todo.id, isEdit: true });
+        }}
+      >
         <span className="task-item-body-text">{todo.title}</span>
-        <input className="task-item-body-input" />
+        <input className="task-item-body-input" value={todo.title} />
       </div>
       <div className="task-item-action ">
         <button className="btn-reset btn-destroy icon"></button>
--- src/pages/TodoPage.jsx
+++ src/pages/TodoPage.jsx
@@ -80,6 +80,21 @@ const TodoPage = () => {
     });
   };
 
+  const handleChangeMode = ({ id, isEdit }) => {
+    setTodos((prevTodos) => {
+      return prevTodos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            isEdit,
+          };
+        }
+
+        return { ...todo, isEdit: false };
+      });
+    });
+  };
+
   return (
     <div>
       TodoPage
@@ -90,7 +105,11 @@ const TodoPage = () => {
         onAddTodo={handleAddTodo}
         onKeyDown={handleKeyDown}
       />
-      <TodoCollection todos={todos} onToggleDone={handleToggleDone} />
+      <TodoCollection
+        todos={todos}
+        onToggleDone={handleToggleDone}
+        onChangeMode={handleChangeMode}
+      />
       <Footer />
     </div>
   );
