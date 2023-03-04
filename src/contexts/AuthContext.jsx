ac990cdeea8d94c68719f8c59552a3b9490c867e
Hsin-tingCHI <chihsinting@gmail.com>
2023-03-04 12:40:07 +0100

    feat: add auth function to context

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,16 +1,68 @@
-import { createContext } from 'react';
+import { login, register } from 'api/auth';
+import { createContext, useState } from 'react';
+import * as jwt from 'jsonwebtoken';
 
 const defaultAuthContext = {
-  isAuthenticated: false, // 使用者是否登入的判斷依據，預設為 false，若取得後端的有效憑證，則切換為 true
-  currentMember: null, // 當前使用者相關資料，預設為 null，成功登入後就會有使用者資料
-  register: null, // 註冊方法
-  login: null, // 登入方法
+  isAuthenticated: false,
+  currentMember: null,
+  register: null,
+  login: null,
   logout: null,
 };
 
 const AuthContext = createContext(defaultAuthContext);
-
-const AuthProvider = ({ children }) => {
+export const AuthProvider = ({ children }) => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [payload, setPayload] = useState(null);
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        currentMember: payload && {
+          id: payload.sub,
+          name: payload.name,
+        },
+        register: async (data) => {
+          const { success, authToken } = await register({
+            username: data.username,
+            email: data.email,
+            password: data.password,
+          });
+          const tempPayload = jwt.decode(authToken);
+          if (tempPayload) {
+            setPayload(tempPayload);
+            setIsAuthenticated(true);
+            localStorage.setItem('authToken', authToken);
+          } else {
+            setPayload(null);
+            setIsAuthenticated(false);
+          }
+          return success;
+        },
+        login: async (data) => {
+          const { success, authToken } = await login({
+            username: data.username,
+            password: data.password,
+          });
+          const tempPayload = jwt.decode(authToken);
+          if (tempPayload) {
+            setPayload(tempPayload);
+            setIsAuthenticated(true);
+            localStorage.setItem('authToken', authToken);
+          } else {
+            setPayload(null);
+            setIsAuthenticated(false);
+          }
+          return success;
+        },
+        logout: () => {
+          localStorage.removeItem('authToken');
+          setPayload(null);
+          setIsAuthenticated(false);
+        },
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
