22a0d04f71594dfd74cca584379ea4b9d05daa4e
Hsin-tingCHI <chihsinting@gmail.com>
2023-03-04 15:00:03 +0100

    feat: add home page navigation

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,19 @@
+import { useAuth } from 'contexts/AuthContext';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const { isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  // 使用者一進來就根據 isAuthenticated 引導到適合的頁面
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/todos');
+    } else {
+      navigate('/login');
+    }
+  }, [navigate, isAuthenticated]);
 };
 
 export default HomePage;
